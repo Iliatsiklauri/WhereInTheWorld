173f3800c4eb0fe5fdee13b4e230d4bd10c6343c
Iliatsiklauri <tsiklauri.ilia26@gmail.com>
2024-02-16 18:31:18 +0400

    commit

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,4 +1,4 @@
-import { useLoaderData } from 'react-router-dom';
+import { Link, useLoaderData } from 'react-router-dom';
 interface CountryData {
   flags?: {
     png: string;
@@ -26,23 +26,127 @@ interface CountryData {
 const About = () => {
   const data = useLoaderData() as CountryData[];
   return (
-    <div>
-      <img src={`${data[0].flags?.png}`} alt="" />
+    <div className="bg-[#202C36] h-screen flex flex-col items-center ">
+      <Link to="/" className="w-[104px] h-8 ">
+        back
+      </Link>
       <div>
-        <h1>saxeli {data[0].name.common}</h1>
-        <h1>vinc ccxovrobs {data[0].demonyms.eng.f}</h1>
-        <h1>1{data[0].population}</h1>
-        <h1>2{data[0].region}</h1>
-        <h1>3{data[0].subregion}</h1>
-        <h1>4 დედაქალაქი{data[0].capital}</h1>
-        <h1>top level domain {data[0].tld}</h1>
-        {/* <h1>6{data[0]?.currencies}</h1> */}
-        <h1>
-          borders
-          {data[0].borders?.map((el, key) => (
-            <p key={key}>{el}</p>
-          ))}
-        </h1>
+        <img src={`${data[0].flags?.png}`} alt="" className="" />
+        <div className="">
+          <div className="flex-row flex items-center justify-start">
+            <p></p>
+            <p
+              className="text-white font-nunito-sans text-14 font-normal leading-8
+"
+            >
+              {data[0].name.common}
+            </p>
+          </div>
+          <div className="">
+            <div className="flex-row flex items-center justify-start">
+              <p
+                className="text-white font-nunito-sans text-14 font-normal leading-32
+"
+              >
+                Native name :
+              </p>
+              <p
+                className="text-white font-nunito-sans text-14 font-normal leading-8
+"
+              >
+                {data[0].demonyms.eng.f}
+              </p>
+            </div>
+            <div className="flex-row flex items-center justify-start">
+              <p
+                className="text-white font-nunito-sans text-14 font-normal leading-32
+"
+              >
+                Population :
+              </p>
+              <p
+                className="text-white font-nunito-sans text-14 font-normal leading-8
+"
+              >
+                {data[0].population}
+              </p>
+            </div>
+            <div className="flex-row flex items-center justify-start">
+              <p
+                className="text-white font-nunito-sans text-14 font-normal leading-32
+"
+              >
+                Region :
+              </p>
+              <p
+                className="text-white font-nunito-sans text-14 font-normal leading-8
+"
+              >
+                {data[0].region}
+              </p>
+            </div>
+            <div className="flex-row flex items-center justify-start">
+              <p
+                className="text-white font-nunito-sans text-14 font-normal leading-32
+"
+              >
+                Subregion :
+              </p>
+              <p
+                className="text-white font-nunito-sans text-14 font-normal leading-8
+"
+              >
+                {data[0].subregion}
+              </p>
+            </div>
+            <div className="flex-row flex items-center justify-start">
+              <p
+                className="text-white font-nunito-sans text-14 font-normal leading-32
+"
+              >
+                Capital :
+              </p>
+              <p
+                className="text-white font-nunito-sans text-14 font-normal leading-8
+"
+              >
+                {data[0].capital}
+              </p>
+            </div>
+            <div className="flex-row flex items-center justify-start">
+              <p
+                className="text-white font-nunito-sans text-14 font-normal leading-32
+"
+              >
+                Top level domain :
+              </p>
+              <p
+                className="text-white font-nunito-sans text-14 font-normal leading-8
+"
+              >
+                {data[0].tld}
+              </p>
+            </div>
+          </div>
+          <div className="h-[90px] flex flex-col gap-4">
+            <p
+              className="text-white font-nunito-sans text-16 font-semibold leading-6
+"
+            >
+              Border Countries:
+            </p>
+            <div className="flex gap-4 flex-wrap w-[340px]">
+              {data[0].borders?.map((el, key) => (
+                <div
+                  className="w-20 h-7 bg-[#2B3844] flex items-center justify-center cursor-pointer"
+                  key={key}
+                >
+                  {el}
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Link, useLoaderData } from 'react-router-dom';
 
-interface Country {
+type Country = {
   name: {
     common: string;
   };
@@ -11,13 +11,13 @@ interface Country {
   population: number;
   region: string;
   capital: string;
-}
+};
 
-const Home: React.FC = () => {
+const Home = () => {
   const data = useLoaderData() as Country[];
 
   const [search, setSearch] = useState('');
-  const [selectedContinent, setSelectedContinent] = useState<string>(''); // Set an appropriate initial value
+  const [selectedContinent, setSelectedContinent] = useState<string>('');
 
   const changeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearch(e.target.value);
@@ -27,19 +27,31 @@ const Home: React.FC = () => {
     setSelectedContinent(e.target.value);
   };
 
+  const continents = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
+
   const filteredData = data.filter(
     (el) =>
       el.name.common.toLowerCase().includes(search.toLowerCase()) &&
       (selectedContinent === '' || el.region === selectedContinent)
   );
-
-  const continents = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
-
+  console.log(filteredData);
   return (
     <div className="bg-[#202C36]">
-      <div className="flex items-center gap-4 p-4">
-        <input type="text" placeholder="Search by country" onChange={changeInput} />
-        <select onChange={changeContinent} value={selectedContinent}>
+      <div className="flex items-start gap-4 p-4 flex-col md:flex-row">
+        <div className="flex w-[343px] items-center justify-around bg-[#2B3844] ">
+          <img src="search.svg" alt="" className="" />
+          <input
+            className="text-white w-[80%] h-12  rounded-sm bg-[#2B3844]"
+            type="text"
+            placeholder="Search by country"
+            onChange={changeInput}
+          />
+        </div>
+        <select
+          onChange={changeContinent}
+          value={selectedContinent}
+          className="mb-[10px] w-[200px] h-12 bg-[#2B3844] rounded-sm "
+        >
           <option value="" disabled hidden>
             Choose a continent
           </option>
@@ -50,7 +62,7 @@ const Home: React.FC = () => {
           ))}
         </select>
       </div>
-      <div className="h-full w-full flex flex-wrap justify-center bg-[#202C36] p-[30px] gap-[90px]">
+      <div className="h-full min-h-screen w-full flex flex-wrap justify-center bg-[#202C36] p-[30px] gap-[90px]">
         {filteredData.map((el, id) => (
           <Link key={id} to={`/${el.name.common}`}>
             <div className="cursor-pointer w-[264px] h-[336px] bg-[#2B3844] rounded-md">
