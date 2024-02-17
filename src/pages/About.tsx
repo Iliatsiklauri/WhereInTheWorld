38e6fc55ed7b69ea02fa2577a620347d349e4761
Iliatsiklauri <tsiklauri.ilia26@gmail.com>
2024-02-17 15:04:41 +0400

    fixed bug

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -26,125 +26,124 @@ interface CountryData {
 const About = () => {
   const data = useLoaderData() as CountryData[];
   return (
-    <div className="bg-[#202C36] h-screen flex flex-col items-center ">
-      <Link to="/" className="w-[104px] h-8 ">
+    <div className="bg-[#202C36] h-screen sm:h-[615px] flex flex-col sm:flex-row items-center justify-center gap-4">
+      <Link
+        to="/"
+        className="w-[104px] h-8 bg-[#2B3844] self-start ml-5 text-center rounded-md"
+      >
         back
       </Link>
-      <div>
-        <img src={`${data[0].flags?.png}`} alt="" className="" />
-        <div className="">
-          <div className="flex-row flex items-center justify-start">
-            <p></p>
-            <p
-              className="text-white font-nunito-sans text-14 font-normal leading-8
+      <img src={`${data[0].flags?.png}`} alt="" className="sm:w-[560px]" />
+      <div className="ml-4">
+        <div className="flex-row flex items-center justify-start">
+          <p
+            className="text-white text-3xl font-extrabold leading-normal
+
 "
-            >
-              {data[0].name.common}
-            </p>
-          </div>
-          <div className="">
-            <div className="flex-row flex items-center justify-start">
-              <p
-                className="text-white font-nunito-sans text-14 font-normal leading-32
+          >
+            {data[0].name.common}
+          </p>
+        </div>
+        <div className="flex-row flex items-center justify-start">
+          <p
+            className="text-white font-nunito-sans text-14 font-normal leading-32
 "
+          >
+            Native name :
+          </p>
+          <p
+            className="text-white font-nunito-sans text-14 font-normal leading-8
+"
+          >
+            {data[0].demonyms.eng.f}
+          </p>
+        </div>
+        <div className="flex-row flex items-center justify-start">
+          <p
+            className="text-white font-nunito-sans text-14 font-normal leading-32
+"
+          >
+            Population :
+          </p>
+          <p
+            className="text-white font-nunito-sans text-14 font-normal leading-8
+"
+          >
+            {data[0].population}
+          </p>
+        </div>
+        <div className="flex-row flex items-center justify-start">
+          <p
+            className="text-white font-nunito-sans text-14 font-normal leading-32
+"
+          >
+            Region :
+          </p>
+          <p
+            className="text-white font-nunito-sans text-14 font-normal leading-8
+"
+          >
+            {data[0].region}
+          </p>
+        </div>
+        <div className="flex-row flex items-center justify-start">
+          <p
+            className="text-white font-nunito-sans text-14 font-normal leading-32
+"
+          >
+            Subregion :
+          </p>
+          <p
+            className="text-white font-nunito-sans text-14 font-normal leading-8
+"
+          >
+            {data[0].subregion}
+          </p>
+        </div>
+        <div className="flex-row flex items-center justify-start">
+          <p
+            className="text-white font-nunito-sans text-14 font-normal leading-32
+"
+          >
+            Capital :
+          </p>
+          <p
+            className="text-white font-nunito-sans text-14 font-normal leading-8
+"
+          >
+            {data[0].capital}
+          </p>
+        </div>
+        <div className="flex-row flex items-center justify-start">
+          <p
+            className="text-white font-nunito-sans text-14 font-normal leading-32
+"
+          >
+            Top level domain :
+          </p>
+          <p
+            className="text-white font-nunito-sans text-14 font-normal leading-8
+"
+          >
+            {data[0].tld}
+          </p>
+        </div>
+        <div className="h-[90px] flex flex-col gap-4">
+          <p
+            className="text-white font-nunito-sans text-16 font-semibold leading-6
+"
+          >
+            Border Countries:
+          </p>
+          <div className="flex gap-4 flex-wrap w-[340px]">
+            {data[0].borders?.map((el, key) => (
+              <div
+                className="w-20 h-7 bg-[#2B3844] flex items-center justify-center cursor-pointer"
+                key={key}
               >
-                Native name :
-              </p>
-              <p
-                className="text-white font-nunito-sans text-14 font-normal leading-8
-"
-              >
-                {data[0].demonyms.eng.f}
-              </p>
-            </div>
-            <div className="flex-row flex items-center justify-start">
-              <p
-                className="text-white font-nunito-sans text-14 font-normal leading-32
-"
-              >
-                Population :
-              </p>
-              <p
-                className="text-white font-nunito-sans text-14 font-normal leading-8
-"
-              >
-                {data[0].population}
-              </p>
-            </div>
-            <div className="flex-row flex items-center justify-start">
-              <p
-                className="text-white font-nunito-sans text-14 font-normal leading-32
-"
-              >
-                Region :
-              </p>
-              <p
-                className="text-white font-nunito-sans text-14 font-normal leading-8
-"
-              >
-                {data[0].region}
-              </p>
-            </div>
-            <div className="flex-row flex items-center justify-start">
-              <p
-                className="text-white font-nunito-sans text-14 font-normal leading-32
-"
-              >
-                Subregion :
-              </p>
-              <p
-                className="text-white font-nunito-sans text-14 font-normal leading-8
-"
-              >
-                {data[0].subregion}
-              </p>
-            </div>
-            <div className="flex-row flex items-center justify-start">
-              <p
-                className="text-white font-nunito-sans text-14 font-normal leading-32
-"
-              >
-                Capital :
-              </p>
-              <p
-                className="text-white font-nunito-sans text-14 font-normal leading-8
-"
-              >
-                {data[0].capital}
-              </p>
-            </div>
-            <div className="flex-row flex items-center justify-start">
-              <p
-                className="text-white font-nunito-sans text-14 font-normal leading-32
-"
-              >
-                Top level domain :
-              </p>
-              <p
-                className="text-white font-nunito-sans text-14 font-normal leading-8
-"
-              >
-                {data[0].tld}
-              </p>
-            </div>
-          </div>
-          <div className="h-[90px] flex flex-col gap-4">
-            <p
-              className="text-white font-nunito-sans text-16 font-semibold leading-6
-"
-            >
-              Border Countries:
-            </p>
-            <div className="flex gap-4 flex-wrap w-[340px]">
-              {data[0].borders?.map((el, key) => (
-                <div
-                  className="w-20 h-7 bg-[#2B3844] flex items-center justify-center cursor-pointer"
-                  key={key}
-                >
-                  {el}
-                </div>
-              ))}
-            </div>
+                {el}
+              </div>
+            ))}
           </div>
         </div>
       </div>
